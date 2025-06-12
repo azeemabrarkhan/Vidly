e7a265f9d7c551009f79e541b603305c62b51ae7
azeemabrarkhan <97536297+azeemabrarkhan@users.noreply.github.com>
2025-06-12 07:29:50 +0200

    add cursor to pagination, add bootstrap script and redirect file

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -10,7 +10,7 @@ const Pagination = (props) => {
 
   return (
     <nav>
-      <ul className="pagination">
+      <ul className="pagination clickable">
         {pages.map((page) => (
           <li
             key={page}
